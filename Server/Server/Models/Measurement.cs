cad4bc6ff072b1402895cba43179532945fc53b6
Fabiola Alston <fabiola.alston@yahoo.com>
2022-05-21 12:43:14 -0600

    Android Studio Changes

---
+++ Server/Server/Models/Measurement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class Measurement
+    {
+        public int idmeasurement { get; set; }
+        public string idpatient { get; set; }
+        public float oxygen { get; set; }
+        public float temperature { get; set; }
+        public float bloodpressure { get; set; }
+        public float pulse { get; set; }
+        public DateTime timestamp { get; set; }
+
+
+    }
+}
---
+++ Server/Server/Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class Patient
+    {
+        public string idpatient { get; set; }
+        public string name { get; set; }
+        public string email;
+        public string birthdate;
+        public string phonenumber;
+        public string location;
+        public float height;
+        public float weight;
+        public string password { get; set; }
+
+    }
+}
